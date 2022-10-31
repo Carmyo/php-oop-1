4e8caa61fe2cd35f1af0fe6295ccab918b5628b7
Carmyo <carmelodicristina@gmail.com>
2022-10-31 15:32:03 +0100

    done

--- index.php
+++ index.php
@@ -10,6 +10,33 @@ class Movie {
     function __construct($_title){
         $this->title = $_title;
     }
+    public function addActor($_cast){
+        $this->cast[] = $_cast;
+    }
 }
 
 ?>
+
+<?php
+$movie = new Movie('The Last Jedi');
+$movie->productionDate = '2017';
+$movie->productionCountry = 'USA';
+$movie->genre = 'Fantasy';
+$movie->rating = '13+';
+$movie->addActor('Daisy Ridley');
+var_dump ($movie);
+
+?>
+
+<?php
+$movie = new Movie('Blade Runner');
+$movie->productionDate = '1982';
+$movie->productionCountry = 'USA';
+$movie->genre = 'sci-fi';
+$movie->rating = '18+';
+$movie->addActor('Harrison Ford');
+var_dump ($movie);
+
+?>
+
+
